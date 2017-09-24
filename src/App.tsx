1d89285570bc51b05a2a25e0adb02737b4f6b2a1
kenjiru <kenjiru.ro@gmail.com>
2017-09-24 22:03:10 +0200

    Changed the primary color.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import * as React from "react";
 import { ReactElement } from "react";
+import { MuiTheme } from "material-ui/styles";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import { greenA700 } from "material-ui/styles/colors";
 
 import Page from "./page/Page";
 
 import "./App.css";
 
+export const appMuiTheme: MuiTheme = {
+    palette: {
+        primary1Color: greenA700,
+    }
+};
+
+const muiTheme: MuiTheme = getMuiTheme(appMuiTheme);
+
 class App extends React.Component {
     public render(): ReactElement<MuiThemeProvider> {
         return (
-            <MuiThemeProvider>
+            <MuiThemeProvider muiTheme={muiTheme}>
                 <div className="app">
                     <Page/>
                 </div>
