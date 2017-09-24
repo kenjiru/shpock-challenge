d6cf899fcb69221edc20dec34a118c1d5fc0299c
kenjiru <kenjiru.ro@gmail.com>
2017-09-24 22:09:29 +0200

    Added the radio buttons and the check boxes to the filters.

--- src/filters/Filters.tsx
+++ src/filters/Filters.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import { Component, ReactElement } from "react";
+import { Toggle } from "material-ui";
+import ClearFix from "material-ui/internal/ClearFix";
 
 import Section from "../section/Section";
 import IconSlider from "../icon-slider/IconSlider";
+import RaisedRadioButton from "../raised-radio-button/RaisedRadioButton";
+import RaisedCheckbox from "../raised-checkbox/RaisedCheckbox";
 
 import "./Filters.css";
 
@@ -22,6 +26,38 @@ class Filters extends Component<IFiltersProps, IFiltersState> {
                             iconRight="dino.png"
                             step={0.20}
                         />
+                    </Section>
+
+                    <Section icon="sorting.png" title="Sort by">
+                        <RaisedRadioButton label="Distance"/>
+                        <RaisedRadioButton label="Date"/>
+                        <ClearFix/>
+                        <RaisedRadioButton label="Price up"/>
+                        <RaisedRadioButton label="Price down"/>
+                    </Section>
+
+                    <Section icon="radius.png" title="Radius" titleRight="everywhere">
+                        <IconSlider iconLeft="home.png" iconRight="world.png" step={0.05}/>
+                    </Section>
+
+                    <Section
+                        icon="flag.png"
+                        title="Search only in my country"
+                        titleRight={<Toggle/>}
+                    />
+
+                    <Section icon="category.png" title="Choose categories">
+                        <RaisedCheckbox label="Everything"/>
+                        <RaisedCheckbox label="New in your area"/>
+                        <RaisedCheckbox label="Fashion and Accessories"/>
+                        <RaisedCheckbox label="Home and Garden"/>
+                        <RaisedCheckbox label="Electronics"/>
+                        <RaisedCheckbox label="Sport, Leisure and Games"/>
+                        <RaisedCheckbox label="Movies, Books and Music"/>
+                        <RaisedCheckbox label="Cars and Motors"/>
+                        <RaisedCheckbox label="Property"/>
+                        <RaisedCheckbox label="Services"/>
+                        <RaisedCheckbox label="Other"/>
                     </Section>
                 </div>
             </div>
