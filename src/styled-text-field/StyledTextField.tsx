37b01b757301c772a11d35d9c6cf35fdd3b3759a
kenjiru <kenjiru.ro@gmail.com>
2017-09-27 14:17:49 +0200

    Created a text field that follows different color cues for validation.

---
+++ src/styled-text-field/StyledTextField.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Component, CSSProperties, ReactElement } from "react";
+import { TextField, TextFieldProps } from "material-ui";
+import { MuiTheme } from "material-ui/styles";
+import muiThemeable from "material-ui/styles/muiThemeable";
+
+class StyledTextField extends Component<IStyledTextFieldProps> {
+
+    public render(): ReactElement<HTMLElement> {
+        let {hasError, muiTheme, ...props} = this.props;
+
+        return (
+            <TextField
+                {...props}
+                underlineStyle={this.getUnderlineStyle()}
+                underlineFocusStyle={this.getUnderlineStyle()}
+                inputStyle={this.getInputStyle()}
+                floatingLabelStyle={this.getInputStyle()}
+            />
+        );
+    }
+
+    private getInputStyle(): CSSProperties {
+        if (this.props.hasError) {
+            return {
+                color: this.props.muiTheme.textField.errorColor
+            };
+        }
+
+        return {};
+    }
+
+    private getUnderlineStyle(): CSSProperties {
+        if (this.props.hasError) {
+            return {
+                borderColor: this.props.muiTheme.textField.errorColor
+            };
+        }
+
+        return {};
+    }
+}
+
+interface IStyledTextFieldProps extends TextFieldProps {
+    hasError?: boolean;
+    muiTheme?: MuiTheme;
+}
+
+export default muiThemeable()(StyledTextField);
